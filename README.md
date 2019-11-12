f7baf4478b051337fbf67a2f440886b24add5639
Jinman Lee <iam@mintplo.me>
2019-11-12 11:16:01 +0900

    Project structure code (Provided) commit

---
+++ README.md
@@ -0,0 +1,109 @@
+## 배달의 한양 어플리케이션 Term Project
+
+2019 HYU 데이터베이스의 원리와 응용(Principle and Application of database) TERM PROJECT
+
+## Implementation
+
+1. 판매자와 구매자 배달대행자를 연결해주는 앱과 서비스를 개발하는 시나리오
+2. 주어진 소스 코드 (app.py) 와 DB를 기반으로 제시되는 시나리오를 만족하는 앱 개발
+
+## Provided
+
+- app.py 및 html 파일
+- 데이터 csv 파일
+    - customer.csv
+    - delivery.csv
+    - menu.csv
+    - owner.csv
+    - pay.csv
+    - store.csv
+
+## Develop Environment
+Based on Mac OSX 10.15 (Catalina)
+
+- Python 3.7.x
+- Flask
+- PyMySQL
+- Pipenv (for Python Virtual Environment)
+- PyCharm Professional Edition
+
+## Project Specifications
+
+### Database
+
+- 데이터는 UTF-8로 인코딩 되어 있음 따라서 DB와 테이블 생성시 기본 인코딩 설정도 UTF-8로 설정해 주어야 함.
+- menu 데이터 로드 시 skipped(warning이 뜸) 되는 에러는 무시해도 됨
+
+```
+LOAD DATA LOCAL INFILE "d:\customer.csv"
+
+INTO table db명.테이블명 FIELDS TERMINATED BY ',' LINES TERMINATED BY '\r\n';
+```
+
+### Customers
+
+- `name`, `passwd`, `phone`, `email`, `address`, `customer_id(PK)`
+- 입력 데이터 파일: `customer.csv`
+- 고객은 메뉴를 선택하여 각 가게(Seller)에 주문을 하는 사람
+
+### Sellers
+
+- `sellers_id(PK)`, `name`, `phone`, `email`, `passwd`
+- 가게 점주
+- 입력 데이터 파일: `seller.csv`
+
+### Delivery
+
+- `del_id(PK)`, `name`, `email`, `passwd`, `area`, `phone`, `available`, `stock`
+- 배달원 정보(area: 배달 지역, available: 배달 가능 여부 1 or 0, stock: 남은 배달 건수)
+- 입력 데이터 파일: `deliverer.csv`
+
+### Stores
+
+- `store_id(PK)`, `address`, `sname`, `phone`, `seller_id(FK)`, `open_time`, `close_time`, `type`
+- 점주가 소유하는 가게, (sname: 가게명, type: 가게 유형)
+- 입력 데이터 파일: `store.csv`
+
+### Menu
+
+- `menu_id(PK)`, `name`, `price`, `event`, `store_id(FK)`
+- 메뉴, (event: 할인율)
+- 입력 데이터 파일: `menu.csv`
+
+### Payment
+
+- `payment_id, customer_id(FK) [PK]`, `pay_num`, `pay_type`
+- 결제, (pay_num: 결제수단의 번호, pay_type: 결제수단 타입, 1 ~ 4)
+- 입력 데이터 파일: `pay.csv`
+
+### Orders
+
+- 주문 테이블은 다음의 컬럼들만 가진다.
+    - `order_id(PK)`, `order_time(주문시간, timestamp)`, `delivery_done(배송 완료 여부, 1 or 0)` = 일반 어트리뷰트
+    - `del_id`, `payment_id`, `customer_id`, `store_id` = FK
+    - **_주문 테이블에 위의 7개 속성(필드)들 이외에 다른 속성을 임의로 추가하지 말것._**
+- FK를 어떤 테이블에서 어떻게 끌어오는지는 각자 생각할 것.
+
+### Orderdetail
+
+- `detail_order_id, order_id(FK) [PK]`, `menu_id(FK)`, `quantity (주문수량)`
+- **_Orderdetail 테이블에는 위의 4개 속성(필드)들 이외에 다른 속성을 임의로 추가하지 말것._**
+- 주문(order) 취소 시 해당 orderdetail도 **_함께 삭제되도록 제약조건을_** 설정해야 함.
+- 가게에서 메뉴 삭제 시 현재 주문 중인 메뉴가 아니라면, 즉 배달 완료 된 메뉴가 삭제 가능해야 하며 삭제 시 Orderdetail의 메뉴 관련 정보는 NULL로 변경되어야 함.
+
+## Running
+
+1. Python Library Installation with Pipenv
+
+```bash
+pipenv install
+pipenv shell
+```
+
+2. Running Flask Application (activated with pipenv)
+
+```bash
+python app.py
+```
+
+3. Go to `localhost:5000`
---
+++ app.py
@@ -0,0 +1,342 @@
+from flask import Flask, redirect, render_template, request
+import pymysql
+import datetime
+
+app = Flask(__name__)
+
+userid = {
+    'id' : None,
+    'email' : None,
+    'passwd' : None,
+    'storesid' : None
+}
+userinfo = [0, 0, 0] # seller, customer, delivery
+menulist = [] # 메뉴 리스트
+
+
+db_connector = {
+    """
+    DB 연결
+    """
+}
+
+@app.route("/")
+def index():
+    userid['email'] = None
+    userid['passwd'] = None
+    userid['id'] = None
+    userinfo[0] = 0
+    userinfo[1] = 0
+    userinfo[2] = 0
+    return render_template("login.html")
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    email = request.form.get('email')
+    passwd = request.form.get('pw')
+    
+    conn = pymysql.connect(**db_connector)
+    cur = conn.cursor(pymysql.cursors.DictCursor)
+
+    sql = f"SELECT seller_id FROM sellers WHERE passwd = '{passwd}' AND email = '{email}'"
+    cur.execute(sql)
+    seller = cur.fetchall()
+    sql = f"SELECT customer_id FROM customers WHERE passwd = '{passwd}' AND email = '{email}'"
+    cur.execute(sql)
+    customer = cur.fetchall()
+    sql = f"SELECT del_id FROM delivery WHERE passwd = '{passwd}' AND email = '{email}'"
+    cur.execute(sql)
+    delivery = cur.fetchall()
+
+    if (not seller) and (not customer) and (not delivery):
+        return render_template('error.html')
+    if seller:
+        userinfo[0] = 1
+    if customer:
+        userinfo[1] = 1
+    if delivery:
+        userinfo[2] = 1
+
+    userid['email'] = email
+    userid['passwd'] = passwd
+
+    if seller :
+        userid['id'] = seller[0]['seller_id']
+    elif customer :
+        userid['id'] = customer[0]['customer_id']
+    elif delivery :
+        userid['id'] = delivery[0]['del_id']
+    else :
+        userid['id'] = None
+    conn.close()
+    return redirect("/login/user")
+
+# 로그인 성공
+@app.route("/login/user", methods=['GET', 'POST'])
+def user():
+    conn = pymysql.connect(**db_connector)
+    """
+    로그인 성공 페이지
+    로그인에 성공한 사용자 정보(판매자, 구매자, 배달대행원)와 이름을 반환
+    사용자 정보(판매자, 구매자, 배달대행원)에 맞는 로그인 성공 페이지를 보여주기 위함
+    """
+    return render_template("user.html", info=userinfo, k=name)
+
+# ========== 판매자 ==========
+
+# 판매자 개인 정보 변경
+@app.route("/login/user/schange", methods=['GET', 'POST'])
+def schange():
+    """
+    판매자 개인 정보 변경 페이지
+    현재 비밀번호와 이름을 확인하기 위함
+    """
+    return render_template("schange.html", info=userinfo, name=sname, passwd=userid['passwd'])
+
+# 판매자 비밀번호 변경
+@app.route("/login/user/schange/pw", methods=['GET', 'POST'])
+def spw():
+    """
+    로그인한 판매자 비밀번호 변경
+    """
+    return redirect("/login/user")
+
+# 판매자 이름 변경
+@app.route("/login/user/schange/name", methods=['GET', 'POST'])
+def schname():
+    """
+    로그인한 판매자 이름 변경
+    """
+    return redirect("/login/user")
+
+# 소유중인 가게 리스트
+@app.route("/login/user/seller", methods=['GET', 'POST'])
+def seller():
+    """
+    소유중인 가게 리스트 페이지
+    로그인한 판매자가 소유중인 가게 리스트를 보여주기 위함
+    """
+    store = cur.fetchall()
+    conn.close()
+    return render_template("seller.html", info=userinfo, store=store)
+
+# 가게 정보, 메뉴 정보, 현재 주문
+@app.route("/login/user/seller/store", methods=['GET', 'POST'])
+def store():
+    sid = request.form.get('sid')
+
+    if sid:
+        userid["storesid"] = sid
+    sid = userid["storesid"]
+    """
+    가게 정보, 메뉴 정보, 현재 주문 페이지
+    가제 정보, 메뉴 정보, 현재 주문을 확인하기 위함
+    """
+    return render_template("store.html", info=userinfo, store=store, menu=menu, sid=sid, order=order)
+
+# 메뉴 이름 변경
+@app.route("/login/user/seller/store/menuchan", methods=['GET', 'POST'])
+def menuchan():
+    """
+    메뉴 이름 변경
+    해당 가게의 새로 입력 받은 메뉴 이름으로 변경하기 위함
+    """
+    return redirect("/login/user/seller/store")
+
+# 메뉴 삭제
+@app.route("/login/user/seller/store/menudel", methods=['GET', 'POST'])
+def menudel():
+    sid = request.form.get('sid')
+    menu = request.form.get('menu')
+    """
+    메뉴 삭제(현재 주문중인 메뉴는 삭제 불가)
+    해당 가게의 메뉴를 삭제하기 위함
+    """
+    return redirect("/login/user/seller/store")
+
+# 메뉴 추가
+@app.route("/login/user/seller/store/menuadd", methods=['GET', 'POST'])
+def menuadd():
+    """
+    메뉴 추가
+    해당 가게의 새로 입력(메뉴명, 가격, 할인율) 받은 메뉴를 추가하기 위함
+    """
+    return redirect("/login/user/seller/store")
+
+# 배달원 할당
+@app.route("/login/user/seller/store/ordercheck", methods=['GET', 'POST'])
+def ordercheck():
+    orderinfo = request.form.get('orderinfo')
+    """
+    배달원 할당
+    현재 주문에 대해 배달 가능한 배달대행원을 최대 5명까지 확인하기 위함(남은 횟수가 높은 순서로 확인)
+    
+    배달 가능한 배달대행원:
+    1. 배달 가능한 지역(가게와 가까운 지역)
+    2. 현재 배달 가능한 상태
+    3. 남은 횟수가 0이 아닌 상태
+    """
+    return render_template("ordercheck.html", info=userinfo, view=deli, orderinfo=orderinfo)
+
+# 현재 주문에 배달원 ID 할당
+@app.route("/login/user/seller/store/ordercheck/real", methods=['GET', 'POST'])
+def orderreal():
+    """
+    현재 주문에 배달원 ID 할당
+    현재 주문에 대해 배달대행원의 배달원 ID를 할당하기 위함
+    """
+    conn.close()
+    return redirect("/login/user/seller/store")
+
+# 주문 취소
+@app.route("/login/user/seller/store/orderdel", methods=['GET', 'POST'])
+def orderdel():
+    """
+    주문 취소
+    현재 주문을 취소하기 위함
+    """
+    return redirect("/login/user/seller/store")
+
+# ========== 구매자 ==========
+
+# 구매자 관리 화면
+@app.route("/login/user/customer", methods=['GET', 'POST'])
+def customer():
+    """
+    구매자 관리 화면 페이지
+    현재 비밀번호와 이름과 주소를 확인하기 위함
+    """
+    return render_template("customer.html", info=userinfo, customer=customer)
+
+# 구매자 비밀번호 변경
+@app.route("/login/user/customer/pw", methods = ['GET', 'POST'])
+def cpw():
+    """
+    로그인한 구매자 비밀번호 변경
+    """
+    return redirect("/login/user")
+
+# 구매자 이름 변경
+@app.route("/login/user/customer/name", methods=['GET', 'POST'])
+def cname():
+    """
+    로그인한 구매자 이름 변경
+    """
+    return redirect("/login/user")
+
+# 구매자 주소 변경
+@app.route("/login/user/customer/address", methods=['GET', 'POST'])
+def addchan():
+    """
+    로그인한 구매자 주소 변경
+    """
+    return redirect("/login/user/customer")
+
+# 구매자 구매화면
+@app.route("/login/user/customer/buy", methods=['GET', 'POST'])
+def buy():
+    """
+    구매화면 페이지
+    로그인한 구매자의 주소로 가게 검색을 하기 위함
+    """
+    return render_template("buy.html", info=userinfo, cus_addr=caddress)
+
+# 고객 기본 주소로 가게 검색
+@app.route("/login/user/schange/consearch", methods=['GET', 'POST'])
+def consearch():
+    """
+    고객 기본 주수로 가게 검색
+    로그인한 구매자의 주소로 부터 가까운 가게 검색을 하기 위함
+    """
+    return render_template("storesearch.html", info=userinfo, store=store)
+
+# 이름으로 가게 검색
+@app.route("/login/user/schange/namesearch", methods=['GET', 'POST'])
+def namesearch():
+    """
+    이름으로 가게 검색
+    가게의 이름으로 검색하기 위함(부분 일치 가능)
+    """
+    return render_template("storesearch.html", info=userinfo, store=store)
+
+# 입력 주소로 가게 검색
+@app.route("/login/user/schange/addresssearch", methods=['GET', 'POST'])
+def addresssearch():
+    """
+    입력 주소로 가게 검색
+    입력한 주소로 가게를 검색하기 위함(부분 일치 가능)
+    """
+    return render_template("storesearch.html", info=userinfo, store=store)
+
+# 가게 정보, 메뉴 정보, 장바구니
+@app.route("/login/user/customer/storebuy", methods=['GET', 'POST'])
+def storebuy():
+    buystoresid = request.form.get('storesid')
+    o_menu = request.form.get('menu')
+    o_num = request.form.get('num')
+    o_sid = request.form.get('sid')
+
+    if not buystoresid:
+        buystoresid = o_sid
+    if o_num and o_num != "0" :
+        menulist.append([o_menu, o_num])
+
+    """
+    가게 정보, 메뉴 정보, 장바구니 페이지
+    가게 정보 및 메뉴 정보를 확인하기 위함
+    """
+    return render_template("order.html", info=userinfo, store=store, menu=menu, menulist=menulist, sid=buystoresid)
+
+# 주문 메뉴 확인, 결제 수단
+@app.route("/login/user/customer/storebuy/pay", methods = ['GET', 'POST'])
+def pay():
+    buystoresid = request.form.get('sid')
+    if not menulist:
+        return render_template("payerror.html")
+
+    """
+    결제 수단
+    로그인한 구매자의 결제 수단 및 결제정보를 확인하여 원하는 방식으로 결제하기 위함
+    """
+    return render_template("realpay.html", info=userinfo, sid=buystoresid, payment=payment, menulist=menulist)
+
+# Order 및 Orderdetail
+@app.route("/login/user/customer/storebuy/pay/done", methods=['GET', 'POST'])
+def realpay():
+    """
+    구매자의 주문을 추가와 주문의 상세 내용을 추가하기 위함
+    """
+    del menulist[:]
+    return redirect("/login/user/customer")
+
+# 주문Order 화면
+@app.route("/login/user/customer/order", methods=['GET', 'POST'])
+def cusorder():
+    """
+    주문Order 화면
+    주문한 가게 이름, 주문한 총 메뉴 수, 결제수단, 주문 시간, 배달 완료 여부를 확인하기 위함
+    """
+    return render_template("payhistory.html", info=userinfo, order=od)
+
+# ========== 배달대행원 ==========
+
+# 현재 배송 중인 주문
+@app.route("/login/user/delivery", methods=['GET', 'POST'])
+def delivery():
+    """
+    현재 OOO님의 배송 중인 주문 페이지
+    가게 이름, 주문자 이름, 주문자 전화번호, 배송지, 주문시간, 배송 완료 여부를 확인하기 위함
+    """
+    return render_template("delivery.html", info=userinfo, order=oorder, deli=deli)
+
+# 배송 완료
+@app.route("/login/user/delivery/deliverydone", methods = ['GET', 'POST'])
+def deliverydone():
+    """
+    배송 완료
+    배달대행원이 배달 완료 시 배달 완료 여부를 배달 완료로 갱신하기 위함
+    """
+    return redirect("/login/user/delivery")
+
+if __name__ == '__main__':
+    app.run(debug=True)
